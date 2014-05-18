1bf06b4cc54eb614059fe96c9a0cad15b9bc9ee5
emesquita <edson.mesquita.ismart@gmail.com>
2014-05-18 16:33:15 -0300

    teste persistencia 2

--- buildAndStartup.sh
+++ buildAndStartup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
+export DATABASE_URL=postgres://multitagplayer:multitagplayer@localhost:5432/multitagplayer  
 mvn clean install -Dmaven.test.skip=true -Pdebug,embedded
 sh multitagplayer-app/target/bin/webapp.sh
