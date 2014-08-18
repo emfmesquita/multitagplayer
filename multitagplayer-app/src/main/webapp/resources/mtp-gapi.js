1deb8e88618c8b97b25e213d2af454c2d25e0ba6
emesquita <emesquita@touchtec.com.br>
2014-08-18 03:23:59 -0300

    oauth como cookie
    Change-Id: I47daeaa7acd40750508375502bb105470ce185d5

--- multitagplayer-app/src/main/webapp/resources/mtp-gapi.js
+++ multitagplayer-app/src/main/webapp/resources/mtp-gapi.js
@@ -1,10 +1,13 @@
 if(typeof mtp == 'undefined') mtp = {};
 (function() {
 	mtp.gapi = {
+		C : {
+			OAUTH_C_KEY : "G_OAUTH_TOKEN"
+		},
 		// The API developer key obtained from the Google Developers Console.
 		developerKey : 'AIzaSyB4OD8l3nKAOUlYX6mz8sisQTFKxLJfr0U',
-		//clientId : '1005266131738-tdv38pudoj50a5jgmbr6khoo3f9fj6pv.apps.googleusercontent.com', // heroko
-		clientId : '1005266131738-3hcds8n24ubv16rmls7vlktd9q1hjj98.apps.googleusercontent.com', // local
+		clientId : '1005266131738-tdv38pudoj50a5jgmbr6khoo3f9fj6pv.apps.googleusercontent.com', // heroko
+		clientIdLocal : '1005266131738-3hcds8n24ubv16rmls7vlktd9q1hjj98.apps.googleusercontent.com', // local
 		// Scope to use to access user's files.
 		scopes : ['https://www.googleapis.com/auth/drive'].join(' '),
 		oauthToken : null,
@@ -12,6 +15,8 @@ if(typeof mtp == 'undefined') mtp = {};
 		_clientAPILoaded : false,
 		_clientAPIAuthed : false,
 		init : function(){
+			mtp.gapi.oauthToken = mtp.gapi._readCookie(mtp.gapi.C.OAUTH_C_KEY);
+
 			if(!mtp.gapi.oauthToken){
 				mtp.gapi._auth();
 			}
@@ -34,15 +39,24 @@ if(typeof mtp == 'undefined') mtp = {};
 				window.location.href = mtp.gapi._getAuthURL();
 			}
 			else{
+				mtp.gapi._createCookie(mtp.gapi.C.OAUTH_C_KEY, mtp.gapi.oauthToken, 59);
 				window.location.hash = "";
 			}
 		},
 		_getAuthURL : function(){
-			var redirect = encodeURIComponent(location.protocol + '//' + location.host + location.pathname)
+			var redirect = encodeURIComponent(location.protocol + '//' + location.host + location.pathname);
+			var rightId = null;
+			if(location.hostname == "localhost"){
+				rightId = mtp.gapi.clientIdLocal;
+			}
+			else{
+				rightId = mtp.gapi.clientId;
+			}
+
 			return "https://accounts.google.com/o/oauth2/auth?"
 					+ "redirect_uri=" + redirect
 					+ "&response_type=token"
-					+ "&client_id=" + encodeURIComponent(mtp.gapi.clientId)
+					+ "&client_id=" + encodeURIComponent(rightId)
 					+ "&scope=" + encodeURIComponent(mtp.gapi.scopes);
 		},
 		_authClientAPI : function(){
@@ -52,6 +66,30 @@ if(typeof mtp == 'undefined') mtp = {};
 			oauthTokenObject.token_type = "Bearer";
 			oauthTokenObject.expires_in = "3600";
 			gapi.auth.setToken(oauthTokenObject);
+		},
+		_createCookie : function(name,value,minutes) {
+			if (minutes){
+				var date = new Date();
+				date.setTime(date.getTime()+(minutes*60*1000));
+				var expires = "; expires="+date.toGMTString();
+			}
+			else{
+				var expires = "";
+			}
+			document.cookie = name+"="+value+expires+"; path=/";
+		},
+		_readCookie : function(name) {
+			var nameEQ = name + "=";
+			var ca = document.cookie.split(';');
+			for(var i=0;i < ca.length;i++) {
+				var c = ca[i];
+				while (c.charAt(0)==' ') c = c.substring(1,c.length);
+				if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+			}
+			return null;
+		},
+		_eraseCookie : function(name) {
+			mtp.gapi._createCookie(name,"",-1);
 		}
 	}
 } ());
